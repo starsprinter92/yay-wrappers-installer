d8046a16f431d21f86ec66f1a7371dc57f861003
guy <ethannathanduvall2013@gmail.com>
2024-06-12 17:02:31 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,13 +6,7 @@ read -p "Do you want to install the wrappers? (y)Yes/(n)No" choice
 
 # giving choices there tasks using
 case $choice in
-[yY]* ) echo "Making dummy install" ;;
-[nN]* ) echo "Installation cancelled" && exit ;;
-exit*)  ;;
-esac
-
-
-git clone https://github.com/starsprinter92/yay-wrappers/
+[yY]* ) echo "Making dummy install" && git clone https://github.com/starsprinter92/yay-wrappers/
 cd yay-wrappers
 chmod +x ./pkginstall.sh
 sudo cp ./pkginstall.sh /usr/bin/pkginstall
@@ -24,3 +18,9 @@ chmod +x ./install.sh
 sudo cp ./install.sh /usr/bin/installer
 cd 
 rm -rf ∼/yay-wrappers
+ ;;
+[nN]* ) echo "Installation cancelled" && exit ;;
+exit*)  ;;
+esac
+
+
