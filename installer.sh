6bf66229b41c45c186ad59ab97528f1223db80cc
guy <ethannathanduvall2013@gmail.com>
2024-06-12 16:51:47 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,30 +1,15 @@
-
 cd
-
 yay -S --noconfirm figlet lolcat
-
 figlet PKGINSTALL INSTALLER | lolcat
-
 git clone https://github.com/starsprinter92/yay-wrappers/
-
 cd yay-wrappers
-
 chmod +x ./pkginstall.sh
-
 sudo cp ./pkginstall.sh /usr/bin/pkginstall
-
 chmod +x update.sh
-
 sudo cp ./update.sh /usr/bin/update
-
 chmod +x ./pkgsearch.sh
-
 sudo cp ./pkgsearch.sh /usr/bin/pkgsearch
-
 chmod +x ./install.sh
-
 sudo cp ./install.sh /usr/bin/installer
-
 cd 
-
 rm -rf ∼/yay-wrappers
