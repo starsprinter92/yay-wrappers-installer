76ece46be7bf133c0f07cea0ae4924b97116a883
guy <ethannathanduvall2013@gmail.com>
2024-06-12 17:04:55 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,10 +3,6 @@ yay -S --noconfirm figlet lolcat
 figlet PKGINSTALL INSTALLER | lolcat
 # prompting for choice
 read -p "Do you want to install the wrappers? (y)Yes/(n)No" choice
-
-# giving choices there tasks using
-case $choice in
-[yY]* ) echo "Making dummy install" && git clone https://github.com/starsprinter92/yay-wrappers/
 cd yay-wrappers
 chmod +x ./pkginstall.sh
 sudo cp ./pkginstall.sh /usr/bin/pkginstall
@@ -18,9 +14,3 @@ chmod +x ./install.sh
 sudo cp ./install.sh /usr/bin/installer
 cd 
 rm -rf ∼/yay-wrappers
- ;;
-[nN]* ) echo "Installation cancelled" && exit ;;
-exit*)  ;;
-esac
-
-
