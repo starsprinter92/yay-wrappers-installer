a63022f788eab184f53ae356970fc93bf8671b51
guy <ethannathanduvall2013@gmail.com>
2024-06-11 20:33:16 -0400

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/starsprinter92/yay-wrappers/
+
+cd yay-wrappers
+
+chmod +x ./pkginstall.sh
+
+sudo cp ./pkginstall.sh /usr/bin/pkginstall
+
+chmod +x update.sh
+
+sudo cp ./update.sh /usr/bin/update
+
+chmod +x ./pkgsearch.sh
+
+sudo cp ./pkgsearch.sh /usr/bin/pkgsearch
